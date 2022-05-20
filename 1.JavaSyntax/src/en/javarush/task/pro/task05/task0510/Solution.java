6f5dbd76c93c679801a810151fba882371e14ff5
dmitri_kolisnitsenko <koldi887@gmail.com>
2022-05-20 22:56:03 +0300

    completed tasks

---
+++ 1.JavaSyntax/src/en/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,26 @@
+package en.javarush.task.pro.task05.task0510;
+
+/* 
+Triangular array
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+            }
+        }
+
+        for (int[] ints : result) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+        }
+}
